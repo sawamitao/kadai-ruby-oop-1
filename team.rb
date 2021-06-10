c3bc48c5c86855b423ce4ca6acdc2d77c6d91cdd
sawami.tao <macaron.3031@icloud.com>
2021-06-10 23:05:03 +0000

    initializeの修正

--- team.rb
+++ team.rb
@@ -1,31 +1,33 @@
 class Team
     attr_accessor :name, :win, :lose, :draw
     
-    def initialize
-       self.name = "名前"
-       self.win = 0
-       self.lose = 0
-       self.draw = 0
+    def initialize(name,win,lose,draw)
+        self.name = name
+        self.win = win
+        self.lose = lose
+        self.draw =draw
         
     end
     def calc_win_rate(win,lose)
         win.to_f/(win+lose).to_f
         
     end
-    def show_team_result(name,win,lose)
-        puts "#{name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate(win,lose)}です。"
+    def show_team_result
+        puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate(win,lose)}です。"
     end
          
 end
-A = Team.new
-B = Team.new
-C = Team.new
-D = Team.new
-E = Team.new
-F = Team.new
-A.show_team_result('Giants',67,45)
-B.show_team_result('Tigers',60,53)
-C.show_team_result('Dragons',60,55)
-D.show_team_result('BayStars',56,58)
-E.show_team_result('Carp',52,56)
-F.show_team_result('Swallows',41,69)
+A = Team.new('Giants',67,45,8)
+B = Team.new('Tigers',60,53,7)
+C = Team.new('Dragons',60,55,5)
+D = Team.new('BayStars',56,58,6)
+E = Team.new('Carp',52,56,12)
+F = Team.new('Swallows',41,69,10)
+A.show_team_result
+B.show_team_result
+C.show_team_result
+D.show_team_result
+E.show_team_result
+F.show_team_result
+
+
